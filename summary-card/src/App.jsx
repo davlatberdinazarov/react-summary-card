6e9d9bc4a944d81afd63816a8fb1e1039b1c6600
davlatberdinazarov <aberdinazarov71@gmail.com>
2024-08-06 10:15:08 +0500

    first commit

---
+++ summary-card/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className="card">
+        <i class="fa-solid fa-house"></i>
+        <h1>hello react js</h1>
+        <img src="/images/iconoir_flash.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default App
